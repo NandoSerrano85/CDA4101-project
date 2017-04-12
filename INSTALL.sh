3a4eda42ad96051e7cb5ff4a6019b61d7de4dd0c
NandoSerrano85 <fserr009@fiu.edu>
2017-04-11 20:01:04 -0400

    added to INSTALL

--- INSTALL.sh
+++ INSTALL.sh
@@ -6,4 +6,4 @@ cd pycuda-VERSION
 ./configure.py --python-exe=/usr/bin/python3 --cuda-root=/usr/local/cuda --cudadrv-lib-dir=/usr/lib/x86_64-linux-gnu --boost-inc-dir=/usr/include --boost-lib-dir=/usr/lib --boost-python-libname=boost_python-py34 --boost-thread-libname=boost_thread --no-use-shipped-boost
 make -j 4
 sudo python setup.py install
-sudo pip install 
+sudo pip install pycuda
