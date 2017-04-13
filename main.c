29355741243f0e4c4cf005b77beb30296e1f97c8
NandoSerrano85 <fserr009@fiu.edu>
2017-04-13 18:03:13 -0400

    changes to main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+/*
+Team: Packers
+PID: 5642858
+Class: CDA4101   Section: U02
+Affirmation:
+"I affirm that this program is entirely
+my own work and none of it is the work
+of any other person."
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <unistd.h>
+#include "bmplib.h"
+
+#define TRUE = 1
+
+// Main function
+int main (int agrc, int agrv){
+    
+}
